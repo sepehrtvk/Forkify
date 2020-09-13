e910af76d7d3fa7b2694d2208cf2674c4bd1e259
Sepehr Tavakoli <sepehrtvk@aut.ac.ir>
2020-09-13 15:29:46 +0430

    Add Search recipe method.

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,4 @@
-import axios from "axios";
-async function getResults(query) {
-  try {
-    const res = await axios(
-      `https://forkify-api.herokuapp.com/api/search?q=${query}`
-    );
-    console.log(res.data.recipes[1]);
-  } catch (error) {
-    alert(error);
-  }
-}
-getResults("chips");
+import Search from './models/Search';
+const search = new Search('pizza');
+search.getResults();
+console.log(search);
