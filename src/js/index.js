2abc65deabf04f35af4358edea4421e55ece0c83
Sepehr Tavakoli <sepehrtvk@aut.ac.ir>
2020-09-13 16:57:36 +0430

    Clear fields after each search.

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,23 @@
-import Search from './models/Search';
-import * as searchView from './views/searchView';
-import {elements} from './views/base';
+import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import { elements } from "./views/base";
 
 const state = {};
 
 const controlSearch = async () => {
-    const query = searchView.getInput();
+  const query = searchView.getInput();
 
-    if(query){
-        state.search = new Search(query); 
-
-
+  if (query) {
+    state.search = new Search(query);
+    searchView.clearInput();
+    searchView.clearResults();
     await state.search.getResults();
     searchView.renderResults(state.search.result);
-    console.log(state.search.result);
-    }
-
+  }
+  console.log(state);
 };
 
-elements.searchFrom.addEventListener('submit',e => {
-    e.preventDefault();
-    controlSearch();
+elements.searchFrom.addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
 });
