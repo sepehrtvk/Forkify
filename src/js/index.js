c2b7b9588398da639c80a42dde27d2aaabacebb4
Sepehr Tavakoli <sepehrtvk@aut.ac.ir>
2020-09-13 15:08:41 +0430

    Make search module.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
-// Global app controller
-import num from './test';
-console.log('aaaaaaaaaa'+num);
+import axios from "axios";
+async function getResults(query) {
+  try {
+    const res = await axios(
+      `https://forkify-api.herokuapp.com/api/search?q=${query}`
+    );
+    console.log(res.data.recipes[1]);
+  } catch (error) {
+    alert(error);
+  }
+}
+getResults("chips");
