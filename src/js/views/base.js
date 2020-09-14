29031eccbe4d27dd6f2c89f89e5534ce970e1dea
Sepehr Tavakoli <sepehrtvk@aut.ac.ir>
2020-09-14 17:52:21 +0430

    Add pages for the search part

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -2,7 +2,8 @@ export const elements = {
   searchFrom: document.querySelector(".search"),
   searchInput: document.querySelector(".search__field"),
   searchResList: document.querySelector(".results__list"),
-  serchRes:  document.querySelector(".results")
+  serchRes:  document.querySelector(".results"),
+  searchResPages: document.querySelector(".results__pages")
 };
 
 export const elementStrings = {
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,30 +1,32 @@
-import {elements} from './base';
+import { elements } from "./base";
 export const getInput = () => elements.searchInput.value;
 
 export const clearInput = () => {
-    elements.searchInput.value='';
+  elements.searchInput.value = "";
 };
 
 export const clearResults = () => {
-    elements.searchResList.innerHTML='';
+  elements.searchResList.innerHTML = "";
+  elements.searchResPages.innerHTML = "";
+
 };
 
 const limitRecipeTitle = (title, limit = 17) => {
-    const newTitle = [];
-    if(title.length > limit){
-        title.split(' ').reduce((acc , cur) => {
-            if(cur.length + acc <= limit){
-                newTitle.push(cur);
-            }
-            return acc+cur.length;
-        },0);
-        return newTitle.join(' ')+' ...';
-    }
-    return title;
-}
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(" ").reduce((acc, cur) => {
+      if (cur.length + acc <= limit) {
+        newTitle.push(cur);
+      }
+      return acc + cur.length;
+    }, 0);
+    return newTitle.join(" ") + " ...";
+  }
+  return title;
+};
 
-const renderRecipe = recipe => {
-    const markup = `
+const renderRecipe = (recipe) => {
+  const markup = `
     <li>
         <a class="results__link" href="#${recipe.recipe_id}">
             <figure class="results__fig">
@@ -36,9 +38,45 @@ const renderRecipe = recipe => {
             </div>
       </a>
     </li>`;
-    elements.searchResList.insertAdjacentHTML('beforeend',markup);
+  elements.searchResList.insertAdjacentHTML("beforeend", markup);
+};
+const createButton = (page, type) => {
+  if (type === 'prev') {
+    return `
+    <button class="btn-inline results__btn--prev" data-goto=${page - 1}>
+    <span>Page ${page - 1}</span>
+    <svg class="search__icon">
+        <use href="img/icons.svg#icon-triangle-left"></use>
+    </svg>
+    </button>`;
+  } else {
+    return `
+    <button class="btn-inline results__btn--next" data-goto=${page + 1}>
+    <span>Page ${page + 1}</span>
+    <svg class="search__icon">
+        <use href="img/icons.svg#icon-triangle-right"></use>
+    </svg>
+    </button>`;
+  }
+};
+const renderButtons = (page, numResults, resPerPage) => {
+  const pages = Math.ceil(numResults / resPerPage);
+  let button;
+
+  if (page === 1 && pages > 1) {
+    button = createButton(page, 'next');
+  } else if (page < pages) {
+    button = `${createButton(page, 'prev')}${createButton(page, 'next')}`;
+  } else if (page === pages && pages > 1) {
+    button = createButton(page, 'prev');
+  }
+  elements.searchResPages.insertAdjacentHTML("afterbegin", button);
 };
 
-export const renderResults = recipes => {
-    recipes.forEach(renderRecipe);
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+  const start = (page - 1) * resPerPage;
+  const end = page * resPerPage;
+  recipes.slice(start, end).forEach(renderRecipe);
+
+  renderButtons(page,recipes.length,resPerPage);
 };
